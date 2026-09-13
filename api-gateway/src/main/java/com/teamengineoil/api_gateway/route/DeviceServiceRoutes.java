d98eb59e755ef7a7e4c89d80450c63790213f446
Priyanshu Kumar <priyanshurazz4@gmail.com>
2026-09-13 16:50:40 +0530

    Added circuit breaker with Resilience4j and Actuator for health and metrics

--- api-gateway/src/main/java/com/teamengineoil/api_gateway/route/DeviceServiceRoutes.java
+++ api-gateway/src/main/java/com/teamengineoil/api_gateway/route/DeviceServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.teamengineoil.api_gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -18,6 +22,16 @@ public class DeviceServiceRoutes {
         return route("device-service")
                 .route(RequestPredicates.path("/api/v1/devices/**"), http())
                 .before(uri("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("deviceServiceCircuitBreaker", URI.create(
+                        "forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userFallbackRoute() {
+        return route("fallbackRoute")
+                .route(RequestPredicates.path("/fallbackRoute"), request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("device-service is down"))
                 .build();
     }
 }
